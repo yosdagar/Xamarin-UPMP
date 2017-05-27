316f810d2abe482a8143f79d6f00d2f9bfc4dea9
yosimar <yosdagar@gmail.com>
2017-05-26 22:48:21 -0500

    taller

---
+++ Lab1/Lab1/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab1
+{
+    [Activity(Label = "Lab1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        Button button;
+        TextView textViewDev;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            button = FindViewById<Button>(Resource.Id.MyButton);
+            textViewDev = FindViewById<TextView>(Resource.Id.textViewDev);
+
+            button.Click += Button_Click1;
+        }
+
+        private void Button_Click1(object sender, EventArgs e)
+        {
+            textViewDev.Text = "Yosdagar";
+            button.Text = "Haz Cambiado el texto";
+        }
+
+        
+    }
+}
---
+++ Lab1/Lab1/obj/Debug/android/src/md5018d703a4e841ff20c7818f3b8d0b16f/MainActivity.java
@@ -0,0 +1,47 @@
+package md5018d703a4e841ff20c7818f3b8d0b16f;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Lab1.MainActivity, Lab1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Lab1.MainActivity, Lab1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
